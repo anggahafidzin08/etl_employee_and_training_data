a8479ee7b9eb0bff2c5be438b2cd3cc283b6c38d
anggahafidzin08 <anggaputrahafidzin@gmail.com>
2024-09-10 06:13:39 +0700

    Initialization push

---
+++ ddl_query/insert_employee_data.sql
@@ -0,0 +1,6 @@
+insert into employee (ID, EmployeeID, FullName, BirthDate, Address)
+values
+	(1, '10105001', 'Ali Anton', '19820819', 'Jakarta Utara')
+	, (2, '10105002', 'Rara Siva', '19820101', 'Mandalika')
+	, (3, '10105003', 'Rini Aini', '19820220', 'Sumbawa Besar')
+	, (4, '10105004', 'Budi', '19820222', 'Mataram Kota');
