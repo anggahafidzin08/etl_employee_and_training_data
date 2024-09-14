7a6b10f63122d12ca7551b6c7cc91ead757f3d64
Angga Hafdz <anggaputrahafidzin@gmail.com>
2024-09-14 13:28:48 +0700

    rework display_employee_position_history

--- query/display_employee_position_history.sql
+++ query/display_employee_position_history.sql
@@ -1,13 +1,30 @@
-select
-	distinct e.EmployeeID
-	, e.FullName
-	, e.BirthDate
-	, e.Address
-	, p.PosID
-	, p.PosTitle
-	, p.StartDate
-	, p.EndDate
-from dbo.employee e
-left join dbo.position p
-	on e.EmployeeID = p.EmployeeID
-order by e.EmployeeID, p.StartDate
+with current_pos as (
+	select
+		distinct e.EmployeeID
+		, e.FullName
+		, e.BirthDate
+		, e.Address
+		, p.PosID
+		, p.PosTitle
+		, p.StartDate
+		, p.EndDate
+		, case when 
+			ROW_NUMBER() over(partition by p.EmployeeID order by p.StartDate DESC) = 1 
+			then 1 else 0 
+		end as is_current_pos
+	from AdventureWorksLT.dbo.employee e
+	left join AdventureWorksLT.dbo.position p
+		on p.EmployeeID = e.EmployeeID
+)
+select 
+	distinct cp.EmployeeID
+	, cp.FullName
+	, cp.BirthDate
+	, cp.Address
+	, cp.PosID
+	, cp.PosTitle
+	, cp.StartDate
+	, cp.EndDate
+from current_pos cp
+where cp.is_current_pos = 1
+	and cp.PosID is not null
